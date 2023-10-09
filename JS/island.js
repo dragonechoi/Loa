9ea2393a75ffb4289ba3e7556db829041d282fc1
hansolbi <johndoe@example.com>
2023-10-09 22:19:50 +0900

    메인 페이지 끝

---
+++ JS/island.js
@@ -0,0 +1,4 @@
+//$("#island_island").hover(
+    //function() {$(this).attr("src","../Images/island_cursor_o/island_island.png");},
+    //function() {$(this).attr("src","../Images/island_cursor_x/island_island.png");
+    //});
